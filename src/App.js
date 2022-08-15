0ae784467324f01cb864f38d16ccfd4b1074b761
Wibowo, Alex <Alex.Wibowo@anz.com>
2022-08-16 09:24:07 +1000

    Code done up to state & behaviour propagation from parent to children (page 61)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
+const title="React";
+
+
+
+
+const List = (props) => 
+  props.list.map((item) => 
+    <div key={item.objectID}>
+      <span>
+        <a href={item.url}>{item.title}</a>
+      </span>
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.points}</span>
+    </div>
+  );
+
+const Search = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <>
+        <label htmlFor='search'>Search: </label>
+        <input id="search" type="text" onChange={props.onSearch}
+          value={props.searchTerm}/>
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Searching for <strong>{props.searchTerm}</strong>.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </>
+  );
+
+};
+
+
+const App = () =>  {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+  }, {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+  } ];
+
+  const [searchTerm, setSearchTerm] = React.useState('React');
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value); 
+  };
+
+  const searchedStories = stories.filter((story) => 
+      story.title
+        .toLowerCase()
+        .includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div>
+      <h1>Hello {title}</h1>
+
+      <Search onSearch={handleChange} searchTerm={searchTerm}/>     
+      <hr/>
+      <List list={searchedStories}/>
     </div>
   );
 }
