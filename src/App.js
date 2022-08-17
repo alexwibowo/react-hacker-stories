34137f26feaf5d4a04f3400aada90386f30888c1
Wibowo, Alex <Alex.Wibowo@anz.com>
2022-08-17 12:12:19 +1000

    Adding comments

--- src/App.js
+++ src/App.js
@@ -4,53 +4,63 @@ import React from 'react';
 
 const title="React";
 
-const List = ({list, onRemoveItem}) => 
-  list.map( (item) => 
+const List = ({ list, onRemoveItem }) =>
+  list.map((item) => (
     <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />
-  );
+  ));
 
-const Item = ({item, onRemoveItem}) =>
-  (
-    <div>
-      <span>
-        <a href={item.url}>{item.title}</a>
-      </span>
-      <span>{item.author}</span>
-      <span>{item.num_comments}</span>
-      <span>{item.points}</span>
-      <span>
-        <button type="button" 
-          onClick={() => {
-            
-            onRemoveItem(item);
-          
-          
-          }}>
-          Dismiss
-        </button>
-      </span>
-    </div>
-  );
+const Item = ({ item, onRemoveItem }) => (
+  <div>
+    <span>
+      <a href={item.url}>{item.title}</a>
+    </span>
+    <span>{item.author}</span>
+    <span>{item.num_comments}</span>
+    <span>{item.points}</span>
+    <span>
+      {/* See how we are using inline handlers here. This is because we want to pass 'item' to the onRemoveItem method */}
+      <button
+        type="button"
+        onClick={() => {
+          onRemoveItem(item);
+        }}
+      >
+        Dismiss
+      </button>
+    </span>
+  </div>
+);
 
-const InputWithLabel = ({id, type="text", value, isFocused, onInputChange, children}) => {
-
+const InputWithLabel = ({
+  id,
+  type = "text",
+  value,
+  isFocused,
+  onInputChange,
+  children,
+}) => {
   const inputRef = React.useRef();
 
-  React.useEffect(function() {
-    if (isFocused && inputRef.current){
-      inputRef.current.focus();
-    }
-
-  }, [isFocused]);
-
+  // here we are using imperative way to focus on a particular DOM. We are using React's useRef for this.
+  React.useEffect(
+    function () {
+      if (isFocused && inputRef.current) {
+        inputRef.current.focus();
+      }
+    },
+    [isFocused]
+  );
 
   return (
     <>
       <label htmlFor={id}>{children}</label>
-      <input id={id} type={type} 
+      <input
+        id={id}
+        type={type}
         ref={inputRef}
         onChange={onInputChange}
-        value={value}/>     
+        value={value}
+      />
     </>
   );
 };
@@ -59,17 +69,12 @@ const Search = ({onSearch, searchTerm}) => {
   return (
       <>
         <InputWithLabel id="search"
-            value={searchTerm}
-            onInputChange={onSearch}>
-            Search :
-          </InputWithLabel>
+          value={searchTerm}
+          isFocused
+          onInputChange={onSearch}>
+          Search :
+        </InputWithLabel>
 
-          <InputWithLabel id="search2"
-            value={searchTerm}
-            isFocused
-            onInputChange={onSearch}>
-            Search :
-          </InputWithLabel>
         <p>
           Searching for <strong>{searchTerm}</strong>.
         </p>
@@ -77,6 +82,7 @@ const Search = ({onSearch, searchTerm}) => {
   );
 };
 
+// custom hook. Look at how the return value follows the React convention for useState - first element array is state value, and the second element array is a setter
 const useSemiPersistentStorage = (key, initialState) => {
 
   const [value, setValue] = React.useState(
@@ -92,54 +98,60 @@ const useSemiPersistentStorage = (key, initialState) => {
 };
 
 
-const App = () =>  {
+// Main application
+const App = () => {
   const initialStories = [
     {
-      title: 'React',
-      url: 'https://reactjs.org/',
-      author: 'Jordan Walke',
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
       num_comments: 3,
       points: 4,
       objectID: 0,
-  }, {
-      title: 'Redux',
-      url: 'https://redux.js.org/',
-      author: 'Dan Abramov, Andrew Clark',
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
       num_comments: 2,
       points: 5,
       objectID: 1,
-  } ];
+    },
+  ];
 
-  const [searchTerm, setSearchTerm] = useSemiPersistentStorage("search", "React");
+  // we use semi persistent storage to remember what use has searched
+  const [searchTerm, setSearchTerm] = useSemiPersistentStorage(
+    "search",
+    "React"
+  );
   const [stories, setStories] = React.useState(initialStories);
-  
-  const handleChange = (event) => {
-    setSearchTerm(event.target.value); 
+
+  const handleChangeSearchTerm = (event) => {
+    setSearchTerm(event.target.value);
   };
 
   const handleRemoveStory = (item) => {
-    const newStories = stories.filter(story =>  
-      story.objectID !== item.objectID
+    const newStories = stories.filter(
+      (story) => story.objectID !== item.objectID
     );
     setStories(newStories);
   };
-  
 
-  const searchedStories = stories.filter((story) => 
-      story.title
-        .toLowerCase()
-        .includes(searchTerm.toLowerCase())
+  // this is interesting.. React knows that there is a dependency between 'searchedStories' and 'searchTerm'.
+  // i.e. when searchTerm changed, this snippet is re-evaluated ?
+  const searchedStories = stories.filter((story) =>
+    story.title.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
   return (
     <div>
       <h1>Hello {title}</h1>
 
-      <Search onSearch={handleChange} searchTerm={searchTerm}/>     
-      <hr/>
-      <List list={searchedStories} onRemoveItem={handleRemoveStory}/>
+      <Search onSearch={handleChangeSearchTerm} searchTerm={searchTerm} />
+      <hr />
+      <List list={searchedStories} onRemoveItem={handleRemoveStory} />
     </div>
   );
-}
+};
 
 export default App;
